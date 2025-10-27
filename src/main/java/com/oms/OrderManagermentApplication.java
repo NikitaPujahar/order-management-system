08d3613d97d22a2b8d41bab0dba27fd4aad24bc6
arun sandip <arun.sandippanigrahi@sigmaconnectivity.se>
2025-10-27 21:23:54 +0100

    Initial commit

---
+++ src/main/java/com/oms/OrderManagermentApplication.java
@@ -0,0 +1,8 @@
+package com.oms;
+
+public class OrderManagermentApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Order Management Application is running...");
+    }
+}
